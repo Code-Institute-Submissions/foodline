756e95bbc907b3689359541370b13b5f25d29423
DADEVFA <davidfarah193@gmail.com>
2021-08-25 18:46:41 +0000

    Updated Product model

--- products/models.py
+++ products/models.py
@@ -8,6 +8,7 @@ class Category(models.Model):
 
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    category_group = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -21,12 +22,17 @@ class Product(models.Model):
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
+    is_ripe = models.BooleanField(default=False, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    weight_price = models.BooleanField(default=False, null=True, blank=True)
+    on_sale = models.BooleanField(default=False, null=True, blank=True)
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    storage_type = models.CharField(max_length=254, null=True, blank=True)
+    inventory_status = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
