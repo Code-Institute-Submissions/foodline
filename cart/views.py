777bf1b5b2062e0d22314188ea0349416b968f34
DADEVFA <davidfarah193@gmail.com>
2021-08-25 19:14:23 +0000

    Finished product ripness logic

--- cart/views.py
+++ cart/views.py
@@ -14,12 +14,24 @@ def add_to_cart(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    ripeness = None
+    if 'product_ripe' in request.POST:
+        ripeness = request.POST['product_ripe']
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    if ripeness:
+        if item_id in list(cart.keys()):
+            if ripeness in cart[item_id]['items_by_ripeness'].keys():
+                cart[item_id]['items_by_ripeness'][ripeness] += quantity
+            else:
+                cart[item_id]['items_by_ripeness'][ripeness] = quantity
+        else:
+            cart[item_id] = {'items_by_ripeness': {ripeness: quantity}}
     else:
-        cart[item_id] = quantity
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
 
     request.session['cart'] = cart
     return redirect(redirect_url)
