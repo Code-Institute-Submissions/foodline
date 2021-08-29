9fc2f36f0af6f369218f438f5b670d225498e98e
DADEVFA <davidfarah193@gmail.com>
2021-08-29 16:03:14 +0000

    Update Country Input to select in checkout

--- checkout/forms.py
+++ checkout/forms.py
@@ -22,16 +22,16 @@ class OrderForm(forms.ModelForm):
             'delivery_address': 'Delivery Address',
             'postcode': 'Postal Code',
             'city': 'City',
-            'country': 'Country',
             'delivery_info': 'Additional info for delivery',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
