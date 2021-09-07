635f39840659a33a8003b6cae7f0cdd1dc938730
DADEVFA <davidfarah193@gmail.com>
2021-09-07 18:24:20 +0000

    Adding defensive code to add product

--- products/views.py
+++ products/views.py
@@ -77,22 +77,26 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add a product to the store """
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+    if request.user.is_superuser:
+        if request.method == 'POST':
+            form = ProductForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Successfully added product!')
+                return redirect(reverse('add_product'))
+            else:
+                messages.error(request,
+                               'Failed to add product. '
+                               'Please ensure the form is valid.')
         else:
-            messages.error(request,
-                           'Failed to add product. '
-                           'Please ensure the form is valid.')
-    else:
+            form = ProductForm()
         form = ProductForm()
-    form = ProductForm()
-    template = 'products/add_product.html'
-    context = {
-        'form': form,
-    }
+        template = 'products/add_product.html'
+        context = {
+            'form': form,
+        }
 
-    return render(request, template, context)
+        return render(request, template, context)
+    else:
+        messages.error(request, 'Ooops, you took the wrong door..')
+        return redirect(reverse('home'))
