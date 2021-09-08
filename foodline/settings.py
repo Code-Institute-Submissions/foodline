154025fd2ab8e4486885c64f1acd25bca1846cb4
DADEVFA <davidfarah193@gmail.com>
2021-09-08 07:16:21 +0000

    Deployment part 2

--- foodline/settings.py
+++ foodline/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-3y57r13n#g0v9e5#do#8r10!yrd(9u6+lbns8%bu3)1zfdguas
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['foodline-1.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,13 +120,16 @@ WSGI_APPLICATION = 'foodline.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
